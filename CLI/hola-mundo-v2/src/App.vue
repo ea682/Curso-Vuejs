71feeb12817d133f050a39ac6bb244da9ff97dba
ea682 <erick.ahumada@lag.cl>
2021-05-16 03:32:59 -0400

    primer componente

---
+++ CLI/hola-mundo-v2/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Titulo texto="Mi banco dinamico 2.0"/>
+</template>
+
+<script>
+import Titulo from './components/Titulo';
+
+export default {
+  name: 'App',
+  components: {
+    Titulo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
