8c321561ca5c9ec7e318183797120995dd222d2d
ea682 <erick.ahumada@lag.cl>
2021-05-16 03:58:29 -0400

    propos entre componentes

--- CLI/hola-mundo-v2/src/App.vue
+++ CLI/hola-mundo-v2/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Titulo texto="Mi banco dinamico 2.0"/>
+  <Cuenta />
 </template>
 
 <script>
 import Titulo from './components/Titulo';
+import Cuenta from './components/Cuenta';
 
 export default {
   name: 'App',
   components: {
-    Titulo
+    Titulo, Cuenta
   }
 }
 </script>
---
+++ CLI/hola-mundo-v2/src/components/Cuenta.vue
@@ -0,0 +1,47 @@
+<template>
+    <h2>Tipo de cuenta: {{cuenta}}</h2>
+    <h2>Saldo: {{saldo}}</h2>
+    <h2>Estado: {{estado ? "Activada" : "Desactivada"}}</h2>
+    <div v-for="(servicio, index) in servicios" :key="index.id"> 
+        {{index+1}}-{{servicio}}
+    </div>
+    <AccionSaldo 
+        texto="Aumentar saldo"
+        @accion="aumentar()"
+    />
+    <AccionSaldo 
+        texto="Disminuir saldo"
+        @accion="disminuir()"
+    />
+</template>
+
+<script>
+import AccionSaldo from './AccionSaldo';
+
+export default {
+
+    components : {
+        AccionSaldo
+    },
+    data(){
+        return {
+            saldo: 1000,
+            cuenta: "Visa",
+            estado: false,
+            servicios: ['giro', 'abono', 'transferencia']
+        }
+    },
+    methods: {
+        aumentar(){
+            this.saldo = this.saldo + 100;
+        },
+        disminuir(){
+            this.saldo = this.saldo - 100;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
